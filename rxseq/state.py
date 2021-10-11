7a5ea6baaae162c904a1dd59d7d865e1259e18df
Ryan Sadler <rrsadler@gmail.com>
2021-10-11 02:26:55 -0700

    update

---
+++ rxseq/state.py
@@ -0,0 +1,45 @@
+from rxseq import common
+
+
+class AppState(common.State):
+    pass
+
+
+class MainState(common.State):
+    pass
+
+
+class ControllerState(common.State):
+    pass
+
+
+class SequencerState(common.State):
+    pass
+
+
+class JackSeqState(common.State):
+    pass
+
+
+class FluidSynthState(common.State):
+    pass
+
+
+class SynthState(common.State):
+    pass
+
+
+class SeqState(common.State):
+    pass
+
+
+class SampleState(common.State):
+    pass
+
+
+class IteratorState(common.State):
+    pass
+
+
+class DataState(common.State):
+    pass
