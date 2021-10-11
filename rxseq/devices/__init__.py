67914b8016f95fd7b44116ce7d625a46e57c3749
Ryan Sadler <rrsadler@gmail.com>
2021-10-11 02:36:59 -0700

    update

--- rxseq/devices/__init__.py
+++ rxseq/devices/__init__.py
@@ -5,5 +5,5 @@ from . import midi_keyboard, midi_drumpad, shuttlepro, footpedal
 def get_midi_driver_from_name(midi_name: str) -> typing.Optional[MidiDriver]:
     device = midi_keyboard.match_name(midi_name)
     if not device:
-        midi_drumpad.match_name(midi_name)
+        device = midi_drumpad.match_name(midi_name)
     return device
